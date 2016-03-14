459d98fe80d06a7a25b5bc8ce5aebba2c85f469b
Stephen Fiser <stephen.fiser@gmail.com>
2016-03-14 14:32:14 -0400

    flips requires

--- lib/althea_passport.rb
+++ lib/althea_passport.rb
@@ -1,13 +1,4 @@
 require "althea_passport/version"
-require "althea_passport/configuration"
-require "althea_passport/empty_token"
-require "althea_passport/empty_user"
-require "althea_passport/identifications"
-require "althea_passport/lab"
-require "althea_passport/safe_token"
-require "althea_passport/safe_user"
-require "althea_passport/token"
-require "althea_passport/user"
 
 module AltheaPassport
 
@@ -25,3 +16,13 @@ module AltheaPassport
 
 
 end
+
+require "althea_passport/configuration"
+require "althea_passport/empty_token"
+require "althea_passport/empty_user"
+require "althea_passport/identifications"
+require "althea_passport/lab"
+require "althea_passport/safe_token"
+require "althea_passport/safe_user"
+require "althea_passport/token"
+require "althea_passport/user"
