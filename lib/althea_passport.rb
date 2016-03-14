2198843ca1457ace2c4b798f1aea369877df4e25
Stephen Fiser <stephen.fiser@gmail.com>
2016-03-14 15:03:08 -0400

    moving junk around

--- lib/althea_passport.rb
+++ lib/althea_passport.rb
@@ -1,13 +1,5 @@
 require "althea_passport/version"
 require "althea_passport/configuration"
-require "althea_passport/empty_token"
-require "althea_passport/empty_user"
-require "althea_passport/identifications"
-require "althea_passport/lab"
-require "althea_passport/safe_token"
-require "althea_passport/safe_user"
-require "althea_passport/token"
-require "althea_passport/user"
 
 module AltheaPassport
 
@@ -26,3 +18,11 @@ module AltheaPassport
 
 end
 
+require "althea_passport/empty_token"
+require "althea_passport/empty_user"
+require "althea_passport/identifications"
+require "althea_passport/lab"
+require "althea_passport/safe_token"
+require "althea_passport/safe_user"
+require "althea_passport/token"
+require "althea_passport/user"
