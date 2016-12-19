e754e94ddf09606e891bb05dba59c4bffcb94663
Stephen Fiser <stephen@bluebear.io>
2016-12-19 00:02:15 -0500

    nope

--- lib/althea_passport/token_coder.rb
+++ lib/althea_passport/token_coder.rb
@@ -4,21 +4,25 @@ require 'active_support/core_ext/hash'
 module AltheaPassport
   class TokenCoder
 
-    def encode(payload, library = JWT)
-      library.encode(payload, secret_key, algorithm)
-    end
+    class << self
 
-    def decode(token, library = JWT)
-      decoded_token = library.decode(token, secret_key, true, { algorithm: algorithm })
-      decoded_token.first.deep_symbolize_keys
-    end
+      def encode(payload, library = JWT)
+        library.encode(payload, secret_key, algorithm)
+      end
 
-    def secret_key
-      AltheaPassport.configuration.jwt_secret_key
-    end
+      def decode(token, library = JWT)
+        decoded_token = library.decode(token, secret_key, true, { algorithm: algorithm })
+        decoded_token.first.deep_symbolize_keys
+      end
 
-    def algorithm
-      'HS256'
+      def secret_key
+        AltheaPassport.configuration.jwt_secret_key
+      end
+
+      def algorithm
+        'HS256'
+      end
+
     end
 
   end
